1f2e3310d7a498a70d8918f9cab3aa8af6360652
Candice Peters <51180851+cep104@users.noreply.github.com>
2020-11-14 16:30:37 -0600

    Update app.rb
    notes

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-    configure do 
-        enable :sessions 
+    configure do
+        enable :sessions
         set :session_secret, "secret"
-    end
+      end 
 
     get '/' do 
         erb :index
-
     end
 
     post '/checkout' do 
-        @session = session
-        @session[:item] = params['item']
+        # binding.pry #use to see what session is 
+        #use to see what happens when you set a session[:item]
+        #what params is 
+        #what params item is
+        
+        @session = session 
+        #set sessions to an instance variable
+        #do this so you can access it in the views
+        @item = params[:item] #set item
+        @session[:item] = @item #set session item
+        @session[:item] = params[:item]
+        adding the item to the session
+
         erb :checkout
     end
-end
